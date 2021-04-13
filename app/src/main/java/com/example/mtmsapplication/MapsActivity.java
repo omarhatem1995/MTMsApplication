dc215ce4224f003bb6ac64a4e8f8e377821053a6
omarhatem1995 <omar.hatem1230@gmail.com>
2021-04-14 00:05:52 +0200

    added fragment destination and google places API

--- app/src/main/java/com/example/mtmsapplication/MapsActivity.java
+++ app/src/main/java/com/example/mtmsapplication/MapsActivity.java
@@ -5,31 +5,47 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.navigation.NavigationView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,
+import java.util.Arrays;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         View.OnClickListener,
         NavigationView.OnNavigationItemSelectedListener {
 
     private ImageView menuIcon;
+    private TextView placeTxt;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
 
     private GoogleMap mMap;
+    private Location destinationPlaceLocation;
+    private Marker marker;
+
+    private String locationFullAddress = "null";
+    private int icon ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,28 +58,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private void initViews(){
+    private void initViews() {
         menuIcon = findViewById(R.id.menu_icon);
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.nav_view);
+        placeTxt = findViewById(R.id.chosen_place_txt);
+
+        icon = R.drawable.ic_marker;
+
+
         navigationView.setNavigationItemSelectedListener(this);
-
-
         menuIcon.setOnClickListener(this);
 
     }
 
-    private void initMaps(){
+    private void initMaps() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        initGooglePlaces();
+    }
+
+    private void initGooglePlaces(){
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyAPRgZ9k_a5Sr2KWtK7TJulY53URh8FkTw");
+        }
+        final AutocompleteSupportFragment autocompleteSupportFragment =
+                (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.destination_fragment);
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,
+                Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS));
+
+        autocompleteSupportFragment.setActivityMode(AutocompleteActivityMode.OVERLAY);
+
+
+        autocompleteSupportFragment.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+
+                        final LatLng latLng = place.getLatLng();
+
+                        locationFullAddress = place.getName();
+                        placeTxt.setText(locationFullAddress);
+                        destinationPlaceLocation = MapsUtils.convertLatLongToLocation(latLng);
+
+                        MapsUtils.zoomMap(destinationPlaceLocation,mMap);
+
+                        marker = MapsUtils.addCustomizedMarker(latLng,"Your Favorite Place",MapsActivity.this,icon,mMap);
+
+
+                    }
+
+                    @Override
+                    public void onError(Status status) {
+                        if (status != null) {
+                            Toast.makeText(MapsActivity.this, status + " is error", Toast.LENGTH_SHORT).show();
+                            Log.d("APIError", status + "");
+                        }
+                    }
+                });
     }
 
 
     private void openSideMenu() {
-                drawerLayout.openDrawer(GravityCompat.START);
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 
     /**
@@ -87,7 +147,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.menu_icon:
                 openSideMenu();
                 break;
---
+++ app/src/main/java/com/example/mtmsapplication/MapsUtils.java
@@ -0,0 +1,78 @@
+package com.example.mtmsapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsUtils {
+
+    public static Location convertLatLongToLocation(LatLng latLng) {
+
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        location.setLatitude(latLng.latitude);
+        location.setLongitude(latLng.longitude);
+
+        return location;
+    }
+
+    public static void zoomMap(Location myLocation, GoogleMap map) {
+
+        Location location = myLocation;
+        if (location != null) {
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(0)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+
+    public static Marker addCustomizedMarker(final LatLng location, String title, final Context context,
+                                             int drawable, GoogleMap googleMap) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(location);
+        markerOptions.title(title).icon((BitmapDescriptorFactory.fromBitmap(drawableToBitmap(context.getDrawable(drawable)))));
+        return googleMap.addMarker(markerOptions);
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+
+
+}
