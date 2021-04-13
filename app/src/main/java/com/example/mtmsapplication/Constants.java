5afbdd5e8a4f34f8118b2604b0a2f0373cb16c50
omarhatem1995 <omar.hatem1230@gmail.com>
2021-04-13 23:17:35 +0200

    added the design of the maps activity

---
+++ app/src/main/java/com/example/mtmsapplication/Constants.java
@@ -0,0 +1,7 @@
+package com.example.mtmsapplication;
+
+public class Constants {
+
+    public static String APP_VERSION ="v"+ BuildConfig.VERSION_NAME;
+
+}
--- app/src/main/java/com/example/mtmsapplication/MapsActivity.java
+++ app/src/main/java/com/example/mtmsapplication/MapsActivity.java
@@ -1,17 +1,33 @@
 package com.example.mtmsapplication;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.common.internal.Constants;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,
+        View.OnClickListener,
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView menuIcon;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     private GoogleMap mMap;
 
@@ -19,10 +35,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        initMaps();
+
+        initViews();
+
+    }
+
+    private void initViews(){
+        menuIcon = findViewById(R.id.menu_icon);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        menuIcon.setOnClickListener(this);
+
+    }
+
+    private void initMaps(){
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+
+    private void openSideMenu() {
+                drawerLayout.openDrawer(GravityCompat.START);
     }
 
     /**
@@ -42,5 +83,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.menu_icon:
+                openSideMenu();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
